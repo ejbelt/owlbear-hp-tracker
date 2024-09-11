bc37e6a4a7ee6de86ac364c6bb3d468476a5e561
ejbelt <ejbelt@mtu.edu>
2024-09-11 13:58:45 -0400

    Going to do temp hp later. Adjusted formatting.

--- src/components/hptracker/Token.tsx
+++ src/components/hptracker/Token.tsx
@@ -151,25 +151,24 @@ export const Token = (props: TokenProps) => {
                     });
                     setData({ ...data, hp: currentData.hp });
                 } else if (key === "tempHp") {
-                        let temp_value = currentData.tempHp
-                        if (Number(value) != 0) {
+                    //Todo: Fix
+                        //let temp_value = currentData.tempHp
+                        //if (Number(value) != 0) {
+                            //let new_hp = currentData.hp + (currentData.tempHp - temp_value);
+                            //updateHpBar(data.hpBar, props.item.id, { ...data, hp: currentData.hp });
+                            //updateText(data.hpOnMap || data.acOnMap, data.canPlayersSee && props.item.visible, props.item.id, {
+                            //    ...data,
+                            //    hp: currentData.hp,
+                            //});
+                            //setData({ ...data, hp: currentData.hp });
+                        //} else {
                             currentData.tempHp = allowNegativNumbers ? Number(value) : Math.max(Number(value), 0);
-                            console.log(currentData.tempHp)
-                            let new_hp = currentData.hp + (currentData.tempHp - temp_value);
-                            console.log(new_hp)
-                            updateHpBar(data.hpBar, props.item.id, { ...data, hp: currentData.hp });
-                            updateText(data.hpOnMap || data.acOnMap, data.canPlayersSee && props.item.visible, props.item.id, {
-                                ...data,
-                                hp: currentData.hp,
-                            });
-                            setData({ ...data, hp: currentData.hp });
-                        } else {
-                            currentData.tempHp = allowNegativNumbers ? Number(value) : Math.max(Number(value), 0);
-                            updateText(data.hpOnMap || data.acOnMap, data.canPlayersSee && props.item.visible, props.item.id, {
-                                ...data,
-                                hp: currentData.hp,
-                            });
-                        }
+                            //updateText(data.hpOnMap || data.acOnMap, data.canPlayersSee && props.item.visible, props.item.id, {
+                            //    ...data,
+                            //    hp: currentData.hp,
+                            //});
+                        //}
+                        currentData.tempHp = allowNegativNumbers ? Number(value) : Math.max(Number(value), 0);
                         updateText(data.hpOnMap || data.acOnMap, data.canPlayersSee && props.item.visible, props.item.id, {
                             ...data,
                             tempHp: currentData.tempHp,
