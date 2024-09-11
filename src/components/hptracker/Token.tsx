9995f17161d5b9717a4515ccdaed206c58da14da
ejbelt <ejbelt@mtu.edu>
2024-09-11 13:47:16 -0400

    NEW TEST!

--- src/components/hptracker/Token.tsx
+++ src/components/hptracker/Token.tsx
@@ -153,9 +153,9 @@ export const Token = (props: TokenProps) => {
                 } else if (key === "tempHp") {
                         let temp_value = currentData.tempHp
                         if (Number(value) != 0) {
-                            currentData.hp = currentData.hp + (currentData.tempHp - temp_value);
+                            let new_hp = currentData.hp + (currentData.tempHp - temp_value);
                             currentData.tempHp = allowNegativNumbers ? Number(value) : Math.max(Number(value), 0);
-                            console.log(currentData.hp)
+                            console.log(new_hp)
                             updateHpBar(data.hpBar, props.item.id, { ...data, hp: currentData.hp });
                             updateText(data.hpOnMap || data.acOnMap, data.canPlayersSee && props.item.visible, props.item.id, {
                                 ...data,
