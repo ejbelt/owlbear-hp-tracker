6193afd2e45c1c49cff88a55811319fc664ef954
ejbelt <ejbelt@mtu.edu>
2024-09-11 09:51:00 -0400

    Revert "Added stuff from another branch to hopefully help."
    This reverts commit 4e7a7e6adc063db6e58779579e773043ccfc0b6b.

--- src/modal.ts
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "react-dom/client";
-import React from "react";
-import "./main.scss";
-import { Modal } from "./components/modal/Modal.tsx";
-
-const root = ReactDOM.createRoot(<HTMLElement>document.querySelector("#app"));
-root.render(React.createElement(Modal));
